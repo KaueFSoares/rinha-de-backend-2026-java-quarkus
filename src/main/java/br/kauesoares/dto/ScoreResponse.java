f35d60b13ef7f5cc154050bfcf49e907290a7c79
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2026-05-10 17:44:15 -0300

    fix: res prop name

--- src/main/java/br/kauesoares/dto/ScoreResponse.java
+++ src/main/java/br/kauesoares/dto/ScoreResponse.java
@@ -1,6 +1,9 @@
 package br.kauesoares.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public final class ScoreResponse {
     public boolean approved;
+    @JsonProperty("fraud_score")
     public float fraudScore;
 }
