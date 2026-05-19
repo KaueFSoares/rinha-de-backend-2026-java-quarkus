92c166dfbcd2e2f04e7bb1998d9e0881d1b1828c
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2026-05-19 09:37:31 -0300

    cleanup

--- src/main/java/br/kauesoares/dto/ScoreResponse.java
+++
@@ -1,9 +0,0 @@
-package br.kauesoares.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public final class ScoreResponse {
-    public boolean approved;
-    @JsonProperty("fraud_score")
-    public float fraudScore;
-}
